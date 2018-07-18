48825669ff583b42885afdd56c50d310e99e90ab
leonidYralsev <yralsevl@gmail.com>
2018-07-18 13:12:12 +0400

    Create project, create Main activity and main_layout

---
+++ app/src/main/java/info/leon/remindme/MainActivity.java
@@ -0,0 +1,13 @@
+package info.leon.remindme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
